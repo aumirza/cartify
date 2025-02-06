a85e494c59cffffe88d9651005b6b94e56cf8ac1
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-02-06 11:32:51 +0530

    feat: replace old products page with new async fetch implementation

---
+++ src/app/(storefront)/products/page.tsx
@@ -0,0 +1,14 @@
+import { fetchProducts } from "@/api/fetchProducts";
+import { ProductsWithFilter } from "@/components/ProductsWithFilter";
+
+export default async function ProductsPage() {
+  const products = await fetchProducts();
+  return (
+    <div className="flex flex-col gap-10 justify-center items-center bg-background">
+      <div className="text-3xl font-semibold pb-2 border-b-2 border-primary">
+        All products
+      </div>
+      <ProductsWithFilter products={products} />
+    </div>
+  );
+}
