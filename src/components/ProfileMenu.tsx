64759bcaecbf41b30d6501c42e206bcb78083d7f
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-02-06 13:14:51 +0530

    feat: remove unused AvatarImage import from ProfileMenu component

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -6,7 +6,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
