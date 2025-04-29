3bc51a4d28486fc0b051cb5e98e1f5c68ad60695
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-04-30 00:45:26 +0530

    feat: add and implement getInitials in profilemenu

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -10,17 +10,19 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { auth, signOut } from "@/lib/auth";
+import { getInitials } from "@/utils/initials";
 
 export async function ProfileMenu() {
   const session = await auth();
   const user = session?.user;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
             {/* <AvatarImage src="/avatars/01.png" alt="@username" /> */}
-            <AvatarFallback>UN</AvatarFallback>
+            <AvatarFallback>{getInitials(user?.name ?? "")}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
