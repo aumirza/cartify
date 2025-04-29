5dc1add9b0bd592027fc585614ba9878843f6d7b
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-04-29 22:54:35 +0530

    fix: add error handling

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -29,7 +29,10 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const client = neon<boolean, boolean>(process.env.DATABASE_URL!);
+const DATABASE_URL = process.env.DATABASE_URL!;
+if (!DATABASE_URL) throw new Error("DATABASE_URL is not defined");
+
+const client = neon<boolean, boolean>(DATABASE_URL);
 const db = drizzle({ client });
 
 export default db;
