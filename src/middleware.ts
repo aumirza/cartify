e719562f4b49b409230e60b136a45195ba4f05a3
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-05-10 01:21:14 +0530

    feat: implement authentication middleware with role-based access control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,30 @@
-export { auth as middleware } from "@/lib/auth";
+import { auth } from "@/lib/auth";
 
-// export const config = {
-//   runtime: "nodejs",
-// };
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+  const isApiAuth = nextUrl.pathname.startsWith("/api/auth");
+  const isAdminRoute = nextUrl.pathname.startsWith("/dashboard");
+  if (isApiAuth) {
+    return;
+  }
+  const isAuthRoute = ["/login", "/signup"].includes(nextUrl.pathname);
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL("/", nextUrl));
+    }
+    return;
+  }
+
+  //   if (!isLoggedIn && !isPublicRoute) {
+  //     return Response.redirect(new URL("/login", nextUrl));
+  //   }
+
+  if (isAdminRoute && req.auth?.user.role !== "admin") {
+    return Response.redirect(new URL("/", nextUrl));
+  }
+  return;
+});
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
